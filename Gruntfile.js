375be80072d09cf704178329543e813e8cec8204
Caleb Rowe <cajrowe@iu.edu>
2020-06-06 12:55:28 -0400

    gitignore and gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -47,4 +47,5 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-concat');
 
   grunt.registerTask('default', ['concat', 'uglify']);
+  grunt.registerTask('production', ['concat', 'uglify']);
 }
