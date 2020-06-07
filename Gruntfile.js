b60baa216be7bfb916ca75b34142cdb469a9b5e3
Caleb Rowe <cajrowe@iu.edu>
2020-06-07 01:24:11 -0400

    test

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,5 @@
 module.exports = function(grunt) {
-  // load plugins 
+  // load plugins
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-concat');
